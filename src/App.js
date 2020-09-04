97c13e59f3eea9bbf995fa20c3955b6eb009c42c
SallyZ12 <szasloff@gmail.com>
2020-09-04 11:56:56 -0400

    tested fetch

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <p> BARR RESERVE </p>
-  );
+  return(
+
+
+    <p> Barr Reserve</p>
+
+)
+
 }
 
 export default App;
