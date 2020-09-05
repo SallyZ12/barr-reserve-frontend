736f1c3e6a9a2aef4226db072565f41d2f3bfad1
SallyZ12 <szasloff@gmail.com>
2020-09-05 19:47:16 -0400

    added path for Home

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
 
     return(
       <div>
+      <Route exact path = '/home' render = {() => <Home home = {this.props.rooms}/>}/>
       <RoomsContainer/>
       </div>
 
