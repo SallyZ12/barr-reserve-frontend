70220cdecffaa16eab3d54cc41d9d8f0c795ed7c
SallyZ12 <szasloff@gmail.com>
2020-09-05 19:59:40 -0400

    removed debugger

--- src/components/Room.js
+++ src/components/Room.js
@@ -5,7 +5,7 @@ import {connect} from 'react-redux'
 
 
 const Room =(props, {rooms}) => {
-debugger
+
   console.log("props", props)
 
   let room = props.rooms.rooms && props.rooms.rooms.filter(room => room.id === parseInt(props.match.params.id))[0]
@@ -14,7 +14,7 @@ debugger
   return (
 
     <div>
-    <h3> HELLO FOR NOW </h3>
+    <h3> {room.room_name} </h3>
     </div>
   )
 
