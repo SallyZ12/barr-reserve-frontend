b817c27ff2c7079e06dc8f123ae5a01b4181b125
SallyZ12 <szasloff@gmail.com>
2020-09-05 19:46:47 -0400

    uncommented console.log

---
+++ src/components/Rooms.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+const Rooms = ({rooms}) => {
+console.log("rooms", rooms)
+
+return (
+
+  <div>
+
+  {rooms && rooms.map(room =>
+    <p key={room.id}>
+
+    <Link to={`/rooms/${room.id}`}> {room.room_name}  </Link><br/>
+    </p>)}
+  </div>
+  )
+}
+
+export default Rooms
