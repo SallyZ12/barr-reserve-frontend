1050ede66339685192acb3cc2487e8e4698a127f
SallyZ12 <szasloff@gmail.com>
2020-09-05 17:37:22 -0400

    new file store.js imported createStore, applyMiddleware, compose, combineReducers from redux and imported thunk, created consts for composeEnhancer and store

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+
+
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+
+export default store
