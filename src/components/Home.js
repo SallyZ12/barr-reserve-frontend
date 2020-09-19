0795d24cbebaf9345070eba7254628b04db912ac
SallyZ12 <szasloff@gmail.com>
2020-09-19 14:27:33 -0400

    changed text of Register to Signup

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,10 +10,10 @@ const Home = (props) => {
     return (
 
       <div className = "Home">
-      <Button variant="outline-primary"><NavLink exactclassname="current" to="/signup">Register</NavLink></Button>
+      <Button variant="outline-primary"><NavLink exactclassname="current" to="/signup">Signup</NavLink></Button>
       <Button variant="outline-primary"><NavLink exactclassname="current" to="/login">Login</NavLink></Button>
         <h2 className = "text-center"> Welcome to The Barrton Reservation System </h2>
-        <h6 className = "text-center"> Register/Login To Begin </h6>
+        <h6 className = "text-center"> Signup/Login To Begin </h6>
         <h4 className = "text-center"> Select a Room </h4>
         <br/>
       <Rooms rooms = {props.home.rooms}/>
