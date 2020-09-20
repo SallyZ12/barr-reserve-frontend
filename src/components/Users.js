6c946a72363b399b8c6bdadcedf062a514cd3287
SallyZ12 <szasloff@gmail.com>
2020-09-20 13:22:44 -0400

    added const Users and mapping of Users

---
+++ src/components/Users.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+const Users = ({users}) => {
+
+function sortByName(data) {
+  return data.sort(function(a,b) {
+    let nameA = a.first_name
+    let nameB = b.first_name
+        let nameSort = nameA.localeCompare(nameB)
+        return nameSort
+      }
+  )
+}
+
+
+return (
+
+  <div>
+    <h5> Registered - Count: {users.users.length} </h5>
+     {users.users && sortByName(users.users).map(user =>
+      <p key={user.id}>
+      <Link to={`/users/${user.id}`}> {user.first_name} {user.last_name}</Link>
+    </p>)}
+    </div>
+  )
+}
+export default Users
