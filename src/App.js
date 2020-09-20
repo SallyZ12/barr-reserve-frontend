1a8f10827c6f24faba1630468070180289d461cd
SallyZ12 <szasloff@gmail.com>
2020-09-20 14:25:35 -0400

    some formatting

--- src/App.js
+++ src/App.js
@@ -41,14 +41,14 @@ class App extends React.Component {
 }
 
 const mapStateToProps = state => {
-  // console.log("MSTP-APP Users:", state.usersReducer)
+  console.log("MSTP-APP Users:", state.usersReducer)
   return ({
     users: state.usersReducer,
     rooms: state.roomsReducer
   })
 }
 
-export default connect(mapStateToProps, { fetchUsers,fetchRooms })(App);
+export default connect(mapStateToProps, { fetchUsers, fetchRooms })(App);
 
 
 /*<Button variant="outline-primary"><NavLink exactclassname="current" to="/signup">Register</NavLink></Button>*/
