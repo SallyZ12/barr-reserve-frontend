3553cefcc52ec1dc312f987c28a04a33f78e7545
SallyZ12 <szasloff@gmail.com>
2020-09-24 18:28:53 -0400

    const Signup with inputs

---
+++ src/components/Signup.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {updateSignupForm} from "../actions/signupForm"
+
+
+const Signup = ({ signupFormData, updateSignupForm, history, handleSubmit, editMode }) => {
+
+// when const signupFormData in the value only need to use the name rather than signupFormData.first_name, etc.
+  const { first_name, last_name, username, email, password, admin} = signupFormData
+
+
+  const handleUserInfoInputChange = event => {
+    const { name, value } = event.target
+
+    const updatedFormInfo = {
+      ...signupFormData,
+      [name]: value
+    }
+    updateSignupForm(updatedFormInfo)
+}
+
+
+  return (
+    <form onSubmit={event => {
+      event.preventDefault()
+      handleSubmit(signupFormData)
+    }}>
+        <h5> Register/Edit </h5>
+      <input  type="text"
+            placeholder="First Name"
+            value={first_name}
+            name="first_name"
+            onChange={handleUserInfoInputChange} />
+            <br/>
+      <input  type="text"
+              placeholder="Last Name"
+              value={last_name}
+              name="last_name"
+              onChange={handleUserInfoInputChange} />
+              <br/>
+       <input type="text"
+              placeholder="username"
+              value={username}
+              name="username"
+              onChange={handleUserInfoInputChange} />
+              <br/>
+      <input  type="text"
+              placeholder="email"
+              value={email}
+              name="email"
+              onChange={handleUserInfoInputChange} />
+              <br/>
+      <select value={apartment}
+              onChange={handleUserInfoInputChange}
+              name="apartment">
+              <option value="PHB">No</option>
+              <option value="5D">Yes</option>
+              </select>
+
+      <input  type="text"
+              placeholder="enter PW >= 8 chars"
+              value={password}
+              name="password"
+              onChange={handleUserInfoInputChange} />
+              <br/>
+              <label>
+              <br/>
+              Admin:
+      <select value={admin}
+              onChange={handleUserInfoInputChange}
+              name="admin">
+              <option value="No">No</option>
+              <option value="Yes">Yes</option>
+      </select>
+              </label>
+          <br/>
+          <br/>
+      <input type="submit" value={editMode ? "Update Player Info" : "Register"}/>
+    </form>
+)}
+
+
+const mapStateToProps = state => {
+// console.log("MSTP-signupFormData:", state.signupFormReducer)
+  return {
+    signupFormData: state.signupFormReducer
+
+  }
+}
+
+export default connect(mapStateToProps, { updateSignupForm })(Signup)
