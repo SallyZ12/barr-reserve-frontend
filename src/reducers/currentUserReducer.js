af9270cac950ad28dd36f102640f60eceb70fd1a
SallyZ12 <szasloff@gmail.com>
2020-09-24 17:45:02 -0400

    added cases SET_CURRENT_USER, ADD_RESERVATION_TO_CURRENT_USER, DELETE_RESERVATION_FROM_CURRENT_USER, ADD_EDITED_USER_TO_CURRENT_USER, and CLEAR_CURRENT_USER

---
+++ src/reducers/currentUserReducer.js
@@ -0,0 +1,19 @@
+export default (state = {}, action) => {
+
+  switch (action.type) {
+
+    case "SET_CURRENT_USER":
+    case "ADD_RESERVATION_TO_CURRENT_USER":
+    case "DELETE_RESERVATION_FROM_CURRENT_USER":
+    case "ADD_EDITED_USER_TO_CURRENT_USER":
+
+      return action.user
+
+    case "CLEAR_CURRENT_USER":
+      return {}
+
+    default:
+      return state
+
+    }
+}
