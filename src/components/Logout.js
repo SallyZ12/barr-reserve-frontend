e600017995eede7483c0e6c8f4a8dd31eb7715d3
SallyZ12 <szasloff@gmail.com>
2020-09-24 19:53:03 -0400

    added const for Logut

---
+++ src/components/Logout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { connect } from 'react-redux'
+import { logout } from "../actions/currentUser.js"
+import Button from 'react-bootstrap/Button';
+import { withRouter } from 'react-router-dom';
+
+const Logout = ({ logout, history }) => {
+  return (
+    <form onSubmit={(event) => {
+        event.preventDefault()
+        logout()
+        history.push("/")
+      }
+    }>
+    <br/><br/>
+      <Button variant="link"><input type="submit" value="Log Out"/></Button>
+    </form>
+  )
+}
+
+export default withRouter(connect(null, { logout } )(Logout))
