66ef4c55a22114829a940e5172ef58b8beb76979
SallyZ12 <szasloff@gmail.com>
2020-09-24 18:05:56 -0400

    set initialState and cases UPDATE_SIGNUP_FORM, RESET_SIGNUP_FORM, and  SET_FORM_DATA_FOR_EDIT

---
+++ src/reducers/signupFormReducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+  first_name: "",
+  last_name: "",
+  username: "",
+  email: "",
+  apartment,
+  password: "",
+  admin: "No"
+}
+
+export default (state = initialState, action)=> {
+  // console.log("sign_up_form:", action.formData)
+  switch(action.type) {
+
+    case "UPDATE_SIGNUP_FORM":
+      return action.formData
+
+    case "RESET_SIGNUP_FORM":
+      return initialState
+
+    case "SET_FORM_DATA_FOR_EDIT":
+      return action.userFormData
+
+    default:
+      return state
+  }
+}
