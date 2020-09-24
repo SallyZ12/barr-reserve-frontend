71fcf58dae3653a2b968e6233ded656b4c8ffe0e
SallyZ12 <szasloff@gmail.com>
2020-09-24 19:55:33 -0400

    imported Logut component and added to NavBar return

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import {Link} from 'react-router-dom'
-// import Logout from '../components/Logout'
+import Logout from '../components/Logout'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 
@@ -11,7 +11,7 @@ let admin1 = currentUser && currentUser.admin === "Yes"
 
   return (
     <div className="NavBar">
-       {/*{Object.keys(currentUser).length !== 0 ? <Logout/> : ""}*/}
+       {Object.keys(currentUser).length !== 0 ? <Logout/> : ""}
        {Object.keys(currentUser).length !== 0 ? <h4> Welcome, {currentUser.first_name} </h4> : "" }
        {Object.keys(currentUser).length !== 0 ? <p> <Link exact='true' to={`/users/${currentUser.id}`}>Reservations</Link></p> :""  }
 
