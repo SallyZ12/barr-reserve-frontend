234678a9a41475c8269525b5a978185898cbe67b
SallyZ12 <szasloff@gmail.com>
2020-09-24 19:31:10 -0400

    added const NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import {Link} from 'react-router-dom'
+// import Logout from '../components/Logout'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
+
+const NavBar =  ({ currentUser}) => {
+
+let admin1 = currentUser && currentUser.admin === "Yes"
+
+  return (
+    <div className="NavBar">
+       {/*{Object.keys(currentUser).length !== 0 ? <Logout/> : ""}*/}
+       {Object.keys(currentUser).length !== 0 ? <h4> Welcome, {currentUser.first_name} </h4> : "" }
+       {Object.keys(currentUser).length !== 0 ? <p> <Link exact='true' to={`/users/${currentUser.id}`}>Reservations</Link></p> :""  }
+
+       {Object.keys(currentUser).length !== 0 && admin1 ? <DropdownButton id="dropdown-basic-button" title="Admin">
+       <Dropdown.Item href = {currentUser && admin1 ? "/rooms": null}>New Room </Dropdown.Item>
+       <Dropdown.Item href = {currentUser && admin1 ? "/users": null}>Owners </Dropdown.Item>
+       </DropdownButton> : ""}
+
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.currentUserReducer
+  }
+}
+
+export default connect(mapStateToProps)(NavBar)
