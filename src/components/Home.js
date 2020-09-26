97cf41297eafabc5388bcda99cc9a3b0dcda8c34
SallyZ12 <szasloff@gmail.com>
2020-09-26 11:22:54 -0400

    commented out console.log

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,13 @@ import Rooms from '../components/Rooms'
 
 
 const Home = (props) => {
-  console.log("home:", props)
+  // console.log("home:", props)
     return (
 
       <div className = "Home">
+
       <Button variant="outline-primary"><NavLink exactclassname="current" to="/signup">Signup</NavLink></Button>
       <Button variant="outline-primary"><NavLink exactclassname="current" to="/login">Login</NavLink></Button>
-      <br/><br/><br/>
 
         <h3 className = "text-center"> Welcome to The Barrton Reservation System </h3>
         <h6 className = "text-center"> Signup/Login To Begin </h6>
