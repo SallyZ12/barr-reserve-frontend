450a76645b8564ff70826753df0edd82a9af587a
SallyZ12 <szasloff@gmail.com>
2020-10-03 12:36:12 -0400

    new code for class EditSignupFormWrapper with comp did mount, com

---
+++ src/components/EditSignupFormWrapper.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Signup from '../components/Signup'
+import { editUser } from '../actions/currentUser'
+import { setFormDataForEdit, resetSignupForm } from '../actions/signupForm'
+import { connect } from 'react-redux'
+
+class EditSignupFormWrapper extends React.Component {
+
+  componentDidMount() {
+    this.props.currentUser && this.props.setFormDataForEdit(this.props.currentUser)
+  }
+
+  componentDidUpdate(prevProps) {
+    this.props.currentUser && !prevProps.currentUser && this.props.setFormDataForEdit(this.props.currentUser)
+  }
+
+  componentWillUnmount(){
+    this.props.resetSignupForm()
+  }
+
+  handleSubmit = (userFormData) => {
+    const { editUser, currentUser, history } = this.props
+
+      editUser({
+        ...userFormData,
+        currentUserId: currentUser.id
+      }, history)
+
+  }
+
+
+render() {
+
+  return <>
+    <Signup editMode handleSubmit={this.handleSubmit} />
+    <br/>
+    </>
+  }
+}
+
+export default connect(null, {editUser, setFormDataForEdit, resetSignupForm})(EditSignupFormWrapper)
