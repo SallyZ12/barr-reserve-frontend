6994f51b59b1c7ed33a6408761355e55720571bc
SallyZ12 <szasloff@gmail.com>
2020-10-04 15:30:14 -0400

    added case for deleting a room

--- src/reducers/roomsReducer.js
+++ src/reducers/roomsReducer.js
@@ -10,8 +10,11 @@ export default function roomsReducer(state = initialState, action) {
      case 'ADD_ROOM':
        return {...state, rooms: [...state.rooms, action.payload]}
 
-       case 'DELETE_ROOM':
+     case 'DELETE_ROOM':
       return {...state, rooms: state.rooms.filter(room=> room.id !== action.payload.id)}
+
+      case "EDIT_CLUB":
+        return {rooms: state.rooms.map(room => room.id === action.room.id ? action.room: room)}
 
      default:
       return state
