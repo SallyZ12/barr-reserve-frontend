2f1c49b4f4e4e89a15938788c2c910b91b17e25f
SallyZ12 <szasloff@gmail.com>
2020-10-04 14:43:24 -0400

    new file with function for creating a new club

---
+++ src/components/NewRoomFormWrapper.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import RoomInput from '../components/RoomInput'
+import { addRoom } from '../actions/addRoom'
+import { connect } from 'react-redux'
+
+const NewRoomFormWrapper = ( {history, addRoom }) => {
+
+  const handleSubmit = (roomData, roomId) => {
+    // console.log("clubData", clubData)
+      addRoom({
+       ...roomData,
+       roomId
+    }, history)
+  }
+
+  return <RoomInput history= {history} handleSubmit={handleSubmit} />
+}
+
+export default connect(null,  { addRoom })(NewRoomFormWrapper)
