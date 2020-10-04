58d1eed03d3bc6508e581068173356cc75ed9a8c
SallyZ12 <szasloff@gmail.com>
2020-10-04 15:41:53 -0400

    added MSTP currentUser, onClick to delete a room, handleDeleteRoom,and let to define admin1 so only admin can see delete link for a room

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 import { NavLink, withRouter } from 'react-router-dom'
 import {connect} from 'react-redux'
-
+import Button from 'react-bootstrap/Button';
+import {deleteRoom} from '../actions/deleteRoom'
 
 const Room =(props, {rooms}) => {
 
@@ -10,22 +11,34 @@ const Room =(props, {rooms}) => {
 
   let room = props.rooms.rooms && props.rooms.rooms.filter(room => room.id === parseInt(props.match.params.id))[0]
 
+  let admin1 = props.currentUser && props.currentUser.admin === "Yes"
+
+  const handleDeleteRoom = (room) => {
+  props.deleteRoom(room.id)
+  props.history.push("/")
+  }
 
   return (
 
     <div>
-    <h3> {room.room_name} </h3>
+    <h3> {room ? room.room_name : null} </h3>
     <p> I'm here </p>
+
+    {admin1 ? <Button variant="link" onClick={()=> handleDeleteRoom(room)}> Delete Room</Button> : ""}
     </div>
   )
 
 }
 
+const mapStateToProps = state => {
+  return ({
+    currentUser: state.currentUserReducer
+  })
+}
 
 
 
-
-export default withRouter(connect()(Room));
+export default withRouter(connect(mapStateToProps, {deleteRoom})(Room));
 
 
 // {room ? room.room_name : null}
