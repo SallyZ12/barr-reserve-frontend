afc7387a7e8983a935200e1f11fb6edef2a4d5ad
SallyZ12 <szasloff@gmail.com>
2020-10-05 19:03:14 -0400

    commented out NavLink and Button imports

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { NavLink }  from 'react-router-dom'
-import Button from 'react-bootstrap/Button'
+// import { NavLink }  from 'react-router-dom'
+// import Button from 'react-bootstrap/Button'
 import Rooms from '../components/Rooms'
 
 
@@ -11,7 +11,7 @@ const Home = (props) => {
 
       <div className = "Home">
 
-        
+
       <br/><br/>
         <h4 className = "text-center"> Welcome to The Barrton Reservation System </h4>
         <h4 className = "text-center"> Select a Room </h4>
