0c07d3be5f64f447b0cec86092057856c3158aab
SallyZ12 <szasloff@gmail.com>
2020-10-05 13:56:38 -0400

    refactored links for signup and login so when signup or login no longer display, some formatting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import Logout from '../components/Logout'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Button from 'react-bootstrap/Button'
-import NavLink from 'react-bootstrap/Navlink'
+
 // import Dropdown from 'react-bootstrap/Dropdown'
 
 const NavBar =  ({ currentUser }) => {
@@ -13,15 +13,15 @@ let admin1 = currentUser && currentUser.admin === "Yes"
 // console.log("NavBar- admin1", admin1)
   return (
     <div className="NavBar">
-       {Object.keys(currentUser).length === 0 ? <p> <Link exact='true' to="/login"> Login</Link></p> : ""}
-       {Object.keys(currentUser).length === 0 ? <p> <Link exact='true' to="/signup"> Signup</Link> </p>: " "}
+
+       {Object.keys(currentUser).length === 0 ? <Button variant='light'> <NavLink exact activeClassName="current" to="/login">Login</NavLink></Button> : ""}
+       {Object.keys(currentUser).length === 0 ? <Button variant='light'> <NavLink exact activeClassName="curent" to="/signup"> Signup</NavLink></Button>: " "}
        {Object.keys(currentUser).length !== 0 ? <Logout/> : ""}
        {Object.keys(currentUser).length !== 0 ? <h4> Welcome, {currentUser.first_name} </h4> : "" }
        {Object.keys(currentUser).length !== 0 ? <p> <Link exact='true' to={`/users/${currentUser.id}`}>Reservations</Link></p> :""  }
 
        {Object.keys(currentUser).length !== 0 && admin1 ? <DropdownButton id="dropdown-basic-button" title="Admin">
-       {/*<Dropdown.Item href = {currentUser && admin1 ? "/rooms": null}>New Room </Dropdown.Item>*/}
-       {/*<Dropdown.Item href = {currentUser && admin1 ? "/users": null}>Owners </Dropdown.Item>*/}
+       
        <Link exact='true' to={"/rooms"}>New Room </Link><br/>
        <Link exact='true' to={"/users"}>Owners </Link>
        </DropdownButton> : ""}
