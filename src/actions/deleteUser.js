bedc7cb5e80c962793ca4d17c17856c96c481da8
SallyZ12 <szasloff@gmail.com>
2020-10-05 18:11:10 -0400

    added action creator for deleteUser

---
+++ src/actions/deleteUser.js
@@ -0,0 +1,16 @@
+export const deleteUser = (userId) => {
+
+  return (dispatch) => {
+    return fetch(`http://localhost:3000/api/v1/users/${userId}`, {
+      method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(user => {
+      if (user.error){
+        alert(room.error)
+      } else {
+      (dispatch({type: 'DELETE_USER', payload: user}))
+  }
+})
+}
+}
