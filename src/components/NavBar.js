c702113796e55df5da05d5e985f2b9e267b467af
SallyZ12 <szasloff@gmail.com>
2020-10-05 13:44:23 -0400

    added code so when login or signout login or signout no longer displayed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import {Link} from 'react-router-dom'
 import Logout from '../components/Logout'
 import DropdownButton from 'react-bootstrap/DropdownButton'
+import Button from 'react-bootstrap/Button'
+import NavLink from 'react-bootstrap/Navlink'
 // import Dropdown from 'react-bootstrap/Dropdown'
 
 const NavBar =  ({ currentUser }) => {
@@ -11,6 +13,8 @@ let admin1 = currentUser && currentUser.admin === "Yes"
 // console.log("NavBar- admin1", admin1)
   return (
     <div className="NavBar">
+       {Object.keys(currentUser).length === 0 ? <p> <Link exact='true' to="/login"> Login</Link></p> : ""}
+       {Object.keys(currentUser).length === 0 ? <p> <Link exact='true' to="/signup"> Signup</Link> </p>: " "}
        {Object.keys(currentUser).length !== 0 ? <Logout/> : ""}
        {Object.keys(currentUser).length !== 0 ? <h4> Welcome, {currentUser.first_name} </h4> : "" }
        {Object.keys(currentUser).length !== 0 ? <p> <Link exact='true' to={`/users/${currentUser.id}`}>Reservations</Link></p> :""  }
