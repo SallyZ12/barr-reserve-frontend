8c233b58b32d18ab8bb23a998f74507173a27127
SallyZ12 <szasloff@gmail.com>
2020-10-05 18:00:07 -0400

    deleted Rooms componanet for route

--- src/containers/RoomsContainer.js
+++ src/containers/RoomsContainer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import Room from '../components/Room'
-import Rooms from '../components/Rooms'
 import NewRoomFormWrapper from '../components/NewRoomFormWrapper'
 import EditRoomFormWrapper from '../components/EditRoomFormWrapper'
 import { Route, Switch } from 'react-router-dom'
@@ -16,7 +15,7 @@ class RoomsContainer extends React.Component {
         <Route exact path="/rooms/:id" render={(routerProps)=> <Room {...routerProps} rooms={this.props.rooms}/>}/>
         <Route exact path='/rooms' component = {NewRoomFormWrapper} />
         <Route exact path="/rooms/:id/edit" render={(rProps)=> <EditRoomFormWrapper {...rProps} rooms={this.props.rooms}/>}/>
-        <Route exact path='/rooms' component = {Rooms} />
+
         </Switch>
       </div>
     )
@@ -25,7 +24,7 @@ class RoomsContainer extends React.Component {
 
 
 const mapStateToProps = state => {
-  // console.log("RoomsContainer state:", state.roomsReducer)
+
   return {
     rooms: state.roomsReducer,
     currentUser: state.currentUser
