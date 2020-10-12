d3d88d7e4343328317af4943c0316953abf0bd85
SallyZ12 <szasloff@gmail.com>
2020-10-12 14:53:35 -0400

    added cases for ADD_RESERVATION_TO_ROOM and DELETE_RESERVATION+FROM_ROOM

--- src/reducers/roomsReducer.js
+++ src/reducers/roomsReducer.js
@@ -16,6 +16,31 @@ export default function roomsReducer(state = initialState, action) {
       case "EDIT_ROOM":
         return {rooms: state.rooms.map(room => room.id === action.room.id ? action.room: room)}
 
+
+        case 'ADD_RESERVATION_TO_ROOM':
+          let maxResId1 = (Math.max(...action.payload.reservations.map(reservation => reservation.id)))
+
+            let rooms4 = state.rooms.map(room => {
+              if (room.room_name === (action.payload.reservations.find(reservation => reservation.id === maxResId1)).reservation_room) {
+                room.room_res.push(action.payload.reservations.find(reservation => reservation.id === maxResId1))}
+                return room
+            })
+
+          return {...state, rooms: rooms4}
+
+          case 'DELETE_RESERVATION_FROM_ROOM':
+
+          let rooms5 = state.rooms.map(room => {
+              if (room.room_res.find(reservation => reservation.id === action.payload)) {
+                    let new_rooms_array = room.room_res.filter(reservation => reservation.id !== action.payload)
+                      room.room_res = new_rooms_array}
+
+                return room
+          })
+          return {...state, rooms: rooms5}
+
+      
+
      default:
       return state
   }
