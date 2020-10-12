24594a56931e2f8b81b34195bc651c2c436b542e
SallyZ12 <szasloff@gmail.com>
2020-10-12 14:41:04 -0400

    added dispatch for method delete with dispatches for  delete reservation, delete reservation from current user, and delete reservation from room

---
+++ src/actions/deleteReservation.js
@@ -0,0 +1,18 @@
+export const deleteReservation = (userId, reservationId) => {
+
+  return (dispatch) => {
+    return fetch(`http://localhost:3000/api/v1/users/${userId}/reservations/${reservationId}`, {
+      method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(user => {
+      if (user.error){
+        alert(user.error)
+      } else {
+      dispatch({type: 'DELETE_RESERVATION', user: user})
+      dispatch({type: 'DELETE_RESERVATION_FROM_CURRENT_USER', user: user})
+      dispatch({type: 'DELETE_RESERVATION_FROM_CLUB', payload: reservationId})
+  }
+})
+}
+}
