77487b97913920d7a9f352a22b8ce58a2eea486f
SallyZ12 <szasloff@gmail.com>
2021-02-28 11:23:53 -0500

    changed room.room_res_date in mapping reservations to render the reservation with date, time and apt

--- src/components/Room.js
+++ src/components/Room.js
@@ -39,7 +39,7 @@ const Room =(props, {rooms}) => {
 
     <ReservationInput currentUser = {props.currentUser} room = {room}/><br/>
 
-           {sortByDate(room.room_res_date).map((reservation =>   <p key={reservation.id}>
+           {sortByDate(room.reservations).map((reservation =>   <p key={reservation.id}>
              {moment(reservation.date).format('MMM DD YYYY')} -- {reservation.hour} -- {reservation.reservation_user_apt} </p>))}
 
     {admin1 ? <Button variant="link" onClick={()=> handleDeleteRoom(room)}> Delete Room</Button> : ""}
