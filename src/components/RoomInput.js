00980347010b63d2b1d091c14d7dd9c6dc57acc4
SallyZ12 <szasloff@gmail.com>
2025-02-23 13:30:26 -0500

    fixed typo in console.log name

--- src/components/RoomInput.js
+++ src/components/RoomInput.js
@@ -7,7 +7,7 @@ import { updateRoomForm } from '../actions/editRoom'
 const RoomInput = ({ roomFormData, updateRoomForm, history, editMode, handleSubmit}) => {
 
   const {roomName} = roomFormData
-   console.log("roomFormdData", roomFormData)
+   console.log("roomFormData", roomFormData)
 
   const handleClubInfoInputChange = event => {
     const { name, value } = event.target
