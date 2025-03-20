662099933c2f043f6e780979d34f23a2bdcb1d64
SallyZ12 <szasloff@gmail.com>
2025-03-20 11:40:21 -0400

    changed 1st line to const currentUserReducer = and added export default currentUserReducer at end from original export default state 1st line to fix eslint warning on compile

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -1,19 +1,44 @@
-export default (state = {}, action) => {
 
-  switch (action.type) {
+const currentUserReducer = (state = {}, action) => {
 
-    case "SET_CURRENT_USER":
-    case "ADD_RESERVATION_TO_CURRENT_USER":
-    case "DELETE_RESERVATION_FROM_CURRENT_USER":
-    case "ADD_EDITED_USER_TO_CURRENT_USER":
+    switch (action.type) {
 
-      return action.user
+      case "SET_CURRENT_USER":
+      case "ADD_RESERVATION_TO_CURRENT_USER":
+      case "DELETE_RESERVATION_FROM_CURRENT_USER":
+      case "ADD_EDITED_USER_TO_CURRENT_USER":
 
-    case "CLEAR_CURRENT_USER":
-      return {}
+        return action.user
 
-    default:
-      return state
+      case "CLEAR_CURRENT_USER":
+        return {}
+
+      default:
+        return state
 
     }
 }
+
+export default currentUserReducer;
+
+
+// Origiinal code prior to eslint warning,changed to eliminate compile warning, post using react-scripts 5.0.1
+// export default (state = {}, action) => {
+
+  // switch (action.type) {
+
+    // case "SET_CURRENT_USER":
+    // case "ADD_RESERVATION_TO_CURRENT_USER":
+    // case "DELETE_RESERVATION_FROM_CURRENT_USER":
+    // case "ADD_EDITED_USER_TO_CURRENT_USER":
+
+      // return action.user
+
+    // case "CLEAR_CURRENT_USER":
+      // return {}
+
+    // default:
+      // return state
+
+    // }
+// }
